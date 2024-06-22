f284a365f0bf2b3f640fc0eca2c436ce4b30c2b8
Gourmouneh <gourmouneh@gmail.com>
2024-06-22 14:17:12 +0300

    1st  commit

---
+++ js/cart/cart.js
@@ -0,0 +1,300 @@
+if (document.readyState == 'loading') {
+  document.addEventListener("DOMContentLoaded", ready)
+} else {
+  ready();
+}
+
+function ready() {
+
+  // remove cart item - accessing buttons
+  var removeCartItemsBtn = document.getElementsByClassName("fa-times-circle");
+
+  for (var i = 0; i < removeCartItemsBtn.length; i++) {
+    var button = removeCartItemsBtn[i];
+    button.addEventListener('click', removeCartItem);
+  }
+  // Quantity change - accesing qty inputs
+
+  var quantityInputs = document.getElementsByClassName("cart-quantity-input");
+
+  for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i];
+    input.addEventListener('change', quantityChanged);
+    input.addEventListener('change', updateSubtotal);
+    input.addEventListener('change', updateCartTotal);
+  }
+
+  // adding items to cart 
+  var cartBody = document.getElementById("cart-body");
+
+  if (cartBody) {
+    if (localStorage.getItem('cartData') != null) {
+      var myCartData = JSON.parse(localStorage.getItem('cartData'));
+
+      for (var i = 0; i < myCartData.length; i++) {
+        var itemData = myCartData[i];
+        if (itemData != null) {
+          var subtotal = itemData.price * itemData.quantity;
+          var currency = itemData.currency;
+
+          var cartRow = document.createElement('tr');
+          cartRow.classList.add("cart-row");
+          cartRow.id = itemData.sku;
+          cartRow.innerHTML = `<td>
+         <i class="far fa-times-circle"></i>
+        </td>
+        <td><img src="https://gourmouneh.github.io/Images/Products/${itemData.image}" alt="" /></td>
+        <td>${itemData.name}</td>
+         <td class="cart-price">${itemData.currency} ${itemData.price}</td>
+         <td><input type="number" value="${itemData.quantity}" class="cart-quantity-input" min="1" step="1"/></td>
+        <td class="cart-item-subtotal">${itemData.currency} ${subtotal}</td>`;
+
+          var cartItems = document.getElementById("cart-body");
+
+          cartItems.appendChild(cartRow);
+
+          cartRow.getElementsByClassName("fa-times-circle")[0].addEventListener('click', removeCartItem);
+
+
+          cartRow.getElementsByClassName("cart-quantity-input")[0].addEventListener('change', updateSubtotal);
+          cartRow.getElementsByClassName("cart-quantity-input")[0].addEventListener('change', quantityChanged);
+          cartRow.getElementsByClassName("cart-quantity-input")[0].addEventListener('change', updateSubtotal);
+          cartRow.getElementsByClassName("cart-quantity-input")[0].addEventListener('change', updateCartTotal);
+
+          updateCartTotal();
+
+        }
+      }
+    }
+
+    updateCartTotal();
+
+    // purchase btn
+    var purchaseBtn = document.getElementById("btn-purchase");
+    purchaseBtn.addEventListener('click', purchaseBtnClicked);
+
+
+    //dont allow decimal
+    document.querySelectorAll('.cart-quantity-input').forEach(function (input) {
+      input.addEventListener('input', function () {
+        // Remove any decimal point and digits following it
+        this.value = this.value.replace(/\D+/g, '');
+      });
+
+      input.addEventListener('change', function () {
+        // Ensure the value is at least the minimum value
+        if (this.value < 1) {
+          this.value = 1;
+        }
+      });
+    });
+  }
+}
+
+// update subtotal - function
+function updateSubtotal(event) {
+  var input = event.target;
+
+  var curCartRow = input.parentElement.parentElement;
+
+  var curPriceElement = curCartRow.getElementsByClassName("cart-price")[0];
+
+  var curPrice = parseFloat(curPriceElement.innerText.replace("$", "").trim());
+  var curSku = curCartRow.id;
+
+  var curQuantity = input.value;
+
+  var subtotal = curQuantity * curPrice;
+
+  curCartRow.getElementsByClassName("cart-item-subtotal")[0].innerText = "$ " + subtotal;
+
+  var existingCartData = JSON.parse(localStorage.getItem('cartData'));
+
+  for (var i = 0; i < existingCartData.length; i++) {
+    if (existingCartData[i].sku == curSku) {
+      existingCartData[i].quantity = parseInt(curQuantity);
+      break;
+    }
+  }
+
+  localStorage.setItem('cartData', JSON.stringify(existingCartData));
+}
+
+// Quantity change - function
+function quantityChanged(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateCartTotal();
+
+}
+
+// remove cart item - function
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+
+  var cartRow = buttonClicked.parentElement.parentElement;
+
+  var cartTitle = cartRow.id;
+
+  var existingCartData = JSON.parse(localStorage.getItem('cartData'));
+
+  for (var i = 0; i < existingCartData.length; i++) {
+    if (existingCartData[i].sku == cartTitle) {
+      existingCartData.splice(i, 1);
+    }
+
+  }
+
+  localStorage.setItem('cartData', JSON.stringify(existingCartData));
+
+  buttonClicked.parentElement.parentElement.remove();
+  updateCartTotal();
+}
+
+// update cart total - function
+function updateCartTotal() {
+  var cartItemContainer = document.getElementsByClassName("cart-items")[0];
+
+  var cartRows = cartItemContainer.getElementsByClassName("cart-row");
+  var total = 0;
+
+  for (var i = 0; i < cartRows.length; i++) {
+    var cartRow = cartRows[i];
+    var subtotalElement = cartRow.getElementsByClassName("cart-item-subtotal")[0];
+
+    var subtotal = parseFloat(subtotalElement.innerText.replace("$", "").trim());
+
+    var total = total + subtotal;
+
+  }
+  total = Math.round(total * 100) / 100;
+
+  document.getElementsByClassName("cart-subtotal")[0].innerText = "$ " + total;
+  document.getElementsByClassName("cart-total")[0].innerText = "$ " + total;
+
+  SetQuantityFlag();
+
+}
+
+// Purchase clicked - functio
+function purchaseBtnClicked(event) {
+  var customerName=$("#name").val();
+  var phoneNumber=$("#number").val();
+  if (localStorage.getItem("cartData") != null) {
+
+    if (customerName == "" && phoneNumber == "") {
+      alert("Please Enter Your Name and Number to proceed");
+      return;
+    } else {
+      if (customerName == "") {
+        alert("Please Enter Your Name to proceed");
+        return;
+      }
+      if (phoneNumber == "") {
+        alert("Please Enter Your Number to proceed");
+        return;
+      }
+    }
+
+    sendEmail();
+
+
+  } else {
+
+    alert("Shopping cart Empty. Please Add Items to Your cart!")
+  }
+}
+
+
+function sendEmail() {
+
+  var cartList = JSON.parse(localStorage.getItem("cartData")) || [];
+  var customerName = $("#name").val();
+  var phoneNumber = $("#number").val();
+  var quantity = 3;
+  var cartTotal = $(".cart-total").text();
+
+  console.log(cartList);
+  console.log(customerName);
+  console.log(phoneNumber);
+  
+  var currentDate = new Date().toLocaleDateString();
+
+  // Create a div element for customer info
+  var customerInfoDiv = document.createElement("div");
+  customerInfoDiv.innerHTML =
+    "<p>Customer Name: " +
+    customerName +
+    "</p>" +
+    "<p>Phone Number: " +
+    phoneNumber +
+    "</p>" +
+    "<p>Total Items: " +
+    quantity +
+    "</p>" +
+    "<p>Total Price: " +
+    cartTotal +
+    "</p>" +
+    "<p>Date: " +
+    currentDate +
+    "</p>";
+
+  // Create a table element
+  var table = document.createElement("table");
+  table.style.borderCollapse = "collapse";
+  table.style.width = "100%";
+
+  var headerRow = document.createElement("tr");
+
+  var headers = ["SKU", "Name","Description" , "Quantity", "Price"];
+
+  headers.forEach(function (headerText) {
+      var headerCell = document.createElement("th");
+      headerCell.style.border = "1px solid black";
+      headerCell.style.padding = "8px";
+      headerCell.textContent = headerText;
+      headerRow.appendChild(headerCell);
+  });
+
+  table.appendChild(headerRow);
+
+  var tbody = document.createElement("tbody");
+
+  cartList.forEach(function (product) {
+    var row = document.createElement("tr");
+
+    headers.forEach(function (headerText) {
+      var cell = document.createElement("td");
+      cell.style.border = "1px solid black";
+      cell.style.padding = "8px";
+      cell.textContent = headerText === "Price" ? "$ " + product[headerText.toLowerCase()] : product[headerText.toLowerCase()];
+      row.appendChild(cell);
+  });
+
+    tbody.appendChild(row);
+  });
+
+  table.appendChild(tbody);
+
+  // Combine customer info div and table
+  var emailContent = customerInfoDiv.outerHTML + table.outerHTML;
+
+
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "gourmouneh@gmail.com",
+    Password: "CCA89F3573A9769E58D3261644246999E1B5",
+    To: "gourmouneh@gmail.com",
+    From: "gourmouneh@gmail.com",
+    Subject: "Order Details from " + customerName,
+    Body: emailContent,
+  }).then(function (message) {
+    alert("Order Sent\nWe will contact you shortly to confirm the details.");
+    sessionStorage.clear();
+    localStorage.removeItem('cartData');
+
+    location.reload();
+  });
+}
---
+++ js/main.js
@@ -0,0 +1,321 @@
+//Cart
+let openShoppingS = document.querySelector('.shoppingS');
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelectorAll('.quantity');
+let listCards = [];
+var activeProducts = [];
+
+(function ($) {
+    "use strict";
+    SetQuantityFlag();
+
+    // //Cart
+    // let openShoppingS = document.querySelector('.shoppingS');
+    // let openShopping = document.querySelector('.shopping');
+    // let closeShopping = document.querySelector('.closeShopping');
+    // let list = document.querySelector('.list');
+    // let listCard = document.querySelector('.listCard');
+    // let body = document.querySelector('body');
+    // let total = document.querySelector('.total');
+    // let quantity = document.querySelectorAll('.quantity');
+
+    // openshopping.addeventlistener('click', () => {
+    //     body.classlist.add('active');
+    // })
+    // openshoppings.addeventlistener('click', () => {
+    //     body.classlist.add('active');
+    // })
+    // closeShopping.addEventListener('click', () => {
+    //     body.classList.remove('active');
+    // })
+
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Fixed Navbar
+    $(window).scroll(function () {
+        if ($(window).width() < 992) {
+            if ($(this).scrollTop() > 45) {
+                $('.fixed-top').addClass('bg-grey shadow');
+            } else {
+                $('.fixed-top').removeClass('bg-grey shadow');
+            }
+        } else {
+            if ($(this).scrollTop() > 45) {
+                $('.fixed-top').addClass('bg-grey shadow').css('top', -45);
+            } else {
+                $('.fixed-top').removeClass('bg-grey shadow').css('top', 0);
+            }
+        }
+    });
+
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo', function () {
+            // Set the URL hash to #home after the scroll animation completes
+            window.location.hash = 'home';
+        });
+        return false;
+    });
+
+    //Add Active to links
+    setActiveClass();
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText: [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+
+    //Get Products List
+    fetchProducts();
+
+})(jQuery);
+
+function fetchProducts() {
+    fetch('./Json/Products.json')
+        .then(response => response.json())
+        .then(products => {
+            const productList = $('#product-list');
+            activeProducts = products.filter(product => product.active);
+            activeProducts.forEach((product, index) => {
+                const productItem = $(`
+                    <div class="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.${index + 1}s">
+                        <div class="product-item">
+                            <div class="position-relative bg-light overflow-hidden">
+                                <img class="img-fluid w-100" src="https://gourmouneh.github.io/Images/Products/${product.image}" alt="">
+                                ${product.new ? '<div class="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">New</div>' : ''}
+                            </div>
+                            <div class="text-center p-4">
+                                <a class="d-block h6 mb-2">${product.name}</a>
+                                <p class="d-block h6 mb-2 description">${product.description}</p>
+                                <span class="text-primary me-1">${product.currency}${product.price}</span>
+                                ${product.discountedPrice != 0 ? '<span class="text-body text-decoration-line-through">${product.discountedPrice}</span>' : ''}
+                            </div>
+                            <div class="d-flex border-top">
+                                <small class="w-100 text-center py-2">
+                                    <a style="cursor: pointer;" class="text-body" onclick="addToCard('${product.sku}')"><i class="fa fa-shopping-bag text-primary me-2"></i>Add to cart</a>
+                                </small>
+                            </div>
+                        </div>
+                    </div>
+                `);
+                productList.append(productItem);
+            });
+        })
+        .catch(error => console.error('Error fetching products:', error));
+}
+
+function SetQuantityFlag() {
+    if (localStorage.getItem('cartData') != null) {
+        var myCartData = JSON.parse(localStorage.getItem('cartData'));
+        var quantityFlag = 0;
+
+        for (var i = 0; i < myCartData.length; i++) {
+            var itemData = myCartData[i];
+            if (itemData != null) {
+                quantityFlag = quantityFlag + itemData.quantity
+
+                quantity.forEach(element => {
+                    element.innerText = quantityFlag;
+                });
+            }
+        }
+    }else {
+        quantity.forEach(element => {
+            element.innerText = 0;
+        });
+    }
+}
+
+function setActiveClass() {
+    // Get all nav links
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+
+    // Function to remove active class from all nav links
+    function removeActiveClasses() {
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+    }
+
+    // Function to add active class to the specified nav link
+    function setActiveClass(link) {
+        removeActiveClasses();
+        link.classList.add('active');
+    }
+
+    // Function to handle the active class based on URL hash
+    function handleHash() {
+        const hash = window.location.hash;
+        if (hash) {
+            const activeLink = document.querySelector(`.navbar-nav .nav-link[href="${hash}"]`);
+            if (activeLink) {
+                setActiveClass(activeLink);
+            }
+        }
+    }
+
+    // Add click event listener to each nav link
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            setActiveClass(link);
+            // Close the navbar dropdown
+            if (navbarCollapse.classList.contains('show')) {
+                new bootstrap.Collapse(navbarCollapse).hide();
+            }
+        });
+    });
+
+    // Set active class based on URL hash on page load
+    handleHash();
+
+    // Optional: Add event listener for hash changes (if hash changes dynamically)
+    window.addEventListener('hashchange', handleHash);
+}
+
+function addToCard(sku) {
+
+    var newCartData = activeProducts.find((product) => product.sku === sku);
+
+    if (localStorage.getItem('cartData') == null) {
+        localStorage.setItem('cartData', '[]');
+    }
+
+    var existingCartData = JSON.parse(localStorage.getItem('cartData'));
+
+    if (existingCartData.length > 0) {
+        for (var i = 0; i < existingCartData.length; i++) {
+            if (sku == existingCartData[i].sku) {
+                existingCartData[i].quantity = existingCartData[i].quantity + 1;
+                break;
+            } else {
+                newCartData.quantity = 1;
+                existingCartData.push(newCartData);
+                break;
+            }
+        }} else {
+            newCartData.quantity = 1;
+            existingCartData.push(newCartData);
+        }
+        localStorage.setItem('cartData', JSON.stringify(existingCartData));
+    SetQuantityFlag();
+    // if (listCards[key] == null) {
+    //     // copy product form list to list card
+    //     console.log(activeProducts);
+    //     listCards[key] = JSON.parse(JSON.stringify(activeProducts[key]));
+    //     listCards[key].quantity = 1;
+    // } else {
+    //     listCards[key].quantity++;
+    // }
+    // reloadCard();
+}
+function SendContactEmail() {
+
+var name=$("#name").val();
+var email=$("#email").val();
+var message= $("#message").val();
+var phoneNumber= $("#phone").val();
+
+if (name == "" && phoneNumber == "") {
+    alert("Please Enter Your Name and Number to proceed");
+    return;
+  } else {
+    if (name == "") {
+      alert("Please Enter Your Name to proceed");
+      return;
+    }
+    if (phoneNumber == "") {
+      alert("Please Enter Your Number to proceed");
+      return;
+    }
+    if (message == "") {
+      alert("Please Enter Your Message to proceed");
+      return;
+    }
+  }
+console.log(message);
+
+
+var currentDate = new Date().toLocaleDateString();
+
+// Create a div element for customer info
+var customerInfoDiv = document.createElement("div");
+customerInfoDiv.innerHTML =
+  "<p>Customer Name: " +
+  name +
+  "</p>" +
+  "<p>Phone Number: " +
+  phoneNumber +
+  "</p>" +
+  "<p>Email: " +
+  email +
+  "</p>" +
+  "<p>Message: " +
+  message +
+  "</p>" +
+  "<p>Date: " +
+  currentDate +
+  "</p>";
+  var emailContent = customerInfoDiv.outerHTML;
+
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "gourmouneh@gmail.com",
+    Password: "CCA89F3573A9769E58D3261644246999E1B5",
+    To: "gourmouneh@gmail.com",
+    From: "gourmouneh@gmail.com",
+    Subject: "Contact Form - From " + name,
+    Body: emailContent,
+  }).then(function (message) {
+    alert("Form Sent\nWe will contact you shortly.");
+    location.reload();
+  });
+}
